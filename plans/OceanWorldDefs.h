3e4bf72b5fcfa4c65881ea5912ed991f1e2f93c6
Mike Dalal <michael.dalal@nasa.gov>
2018-12-19 20:38:41 -0800

    All PLEXIL compiles

--- plans/OceanWorldDefs.h
+++ plans/OceanWorldDefs.h
@@ -2,6 +2,9 @@
 #define DigEnergyConsumptionRate 50.0
 #define FilmInterval 30
 #define SenseDigInterval 60
+#define DigTrenchTimeout 10
 
 #define StubTrue true
 #define StubFalse false
+
+#define InstrumentId Integer
