c01da5fad56c7a4326d64ab817d7e94dc1a65e67
Kutsyy, Albert (ARC-TI)[UNIVERSITIES SPACE RESEARCH ASSOCIATION] <albert.kutsyy@nasa.gov>
2020-08-12 17:26:08 -0700

    Added Checkpoints to everything, currently untested

--- src/plans/plexil_defs.h
+++ src/plans/plexil_defs.h
@@ -37,7 +37,7 @@
 #define TILT_MIN  -45
 #define TILT_MAX  45
 
-// Maximum number of times to attempt getting the target from ground
-#define MAX_TARGET_GROUND_ATTEMPTS 2
+// Maximum number of crashes before assuming something has gone very wrong and attempting to offload all data
+#define MAX_CRASHES 10
 
 #endif
