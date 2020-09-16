2185a160758c108c974190f9c38b1984e86532ea
Mike Dalal <michael.dalal@nasa.gov>
2020-09-15 20:47:18 -0700

    removed unworking Timeout option from SynchronousCommand

--- src/plans/plan-interface.h
+++ src/plans/plan-interface.h
@@ -9,17 +9,31 @@
 
 // Lander commands
 
+// The following commands perform only the path planning for the given activity.
+
 Command dig_circular (Real x, Real y, Real depth, Real ground_pos,
-                      Boolean radial);
+                      Boolean radial); // true means parallel to lander arm
+
 Command dig_linear (Real x, Real y, Real depth, Real length,
                     Real ground_pos);
+
+// This dumps the scoop at the given location; can be used for removing tailings
+// as well as delivering a sample to the receptacle.
 Command deliver_sample (Real x, Real y, Real z);
+
 Command grind (Real x, Real y, Real depth, Real length, Real ground_pos);
-Command guarded_move (Real x, Real y, Real z,
-                      Real dir_x, Real dir_y, Real dir_z,
-                      Real search_distance);
+
+Command guarded_move (Real x, Real y, Real z,  // starting point
+                      Real dir_x, Real dir_y, Real dir_z,  // normal vector
+                      Real search_distance);  // depth along normal vector
+
+Command unstow();  // move from stowed position to a "ready" position
+
+// move from "ready" position to stowed position; REQUIRES unstow() first
 Command stow();
-Command unstow();
+
+
+// This command sends the planned trajectory to Gazebo, i.e. moves real arm
 Command publish_trajectory();
 
 // Utility commands
