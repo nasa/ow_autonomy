1533e7a3c2838ee0465c5280bd699768c270ccfe
Keeganfn <keegan.nave@gmail.com>
2021-07-01 17:11:41 -0700

    fixed weird spacing and print statements

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['rqt_plexil_planner'],
-    package_dir={'': 'src'},
-    requires=['std_msgs', 'rospy']
+  packages=['rqt_plexil_planner'],
+  package_dir={'': 'src'},
+  requires=['std_msgs', 'rospy']
 )
 
 setup(**setup_args)
