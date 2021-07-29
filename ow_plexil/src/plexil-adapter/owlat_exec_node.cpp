a95a13831da5b706a479144207a1980d50ad20c7
Michael Dalal <michael.dalal@nasa.gov>
2021-07-29 12:27:27 -0700

    adds owlat_exec_node, built by default; node crashes after running a plan

---
+++ ow_plexil/src/plexil-adapter/owlat_exec_node.cpp
@@ -0,0 +1,34 @@
+// The Notices and Disclaimers for Ocean Worlds Autonomy Testbed for Exploration
+// Research and Simulation can be found in README.md in the root directory of
+// this repository.
+
+// ROS
+#include <ros/ros.h>
+#include <ros/package.h>
+
+#include "PlexilPlanSelection.h"
+#include "OwExecutive.h"
+#include "OWLATSimInterface.h"
+
+int main(int argc, char* argv[])
+{
+  // Initializations
+  ros::init(argc, argv, "ow_exec_node");
+  std::string initial_plan = "None";
+
+  //checking if there is a plan given
+  if(argc == 2 && std::string(argv[1]).compare("None") != 0) {
+    std::string plan(argv[1]);
+    initial_plan = plan;
+  }
+  if (! OwExecutive::instance()->initialize()) {
+    ROS_ERROR("Could not initialize Plexil executive, shutting down.");
+    return 1;
+  }
+  OWLATSimInterface::instance()->initialize();
+  PlexilPlanSelection plan_selection;
+  plan_selection.initialize(initial_plan); //initialize pubs, subs, etc
+  plan_selection.start(); //begin control loop
+  ros::spin();
+  return 0;  // We never actually get here!
+}
