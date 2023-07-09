aba42b920426878f1caa068108ab8f09bdf524ed
Michael Dalal <michael.dalal@nasa.gov>
2023-07-08 20:34:44 -0700

    Added more checkpoint interface

--- ow_plexil/src/plans/common/common-interface.h
+++ ow_plexil/src/plans/common/common-interface.h
@@ -109,7 +109,8 @@ Boolean Lookup UsingOWLAT;
 // fine-grained control of concurrency.
 Boolean Lookup Running (String operation_name);
 
-// Query the goal status of the ROS action corresponding to a given library action
+// Query the goal status of the ROS action corresponding to a given
+// library action.
 Integer Lookup ActionGoalStatus (String action_name);
 
 // Function
@@ -121,22 +122,74 @@ Boolean Lookup AnglesEquivalent (Real deg1, Real deg2, Real tolerance);
 // Predefined, PLEXIL variable for current time.
 Real Lookup time;
 
-// String operations
-String Lookup ToString(...);
-Boolean Lookup StringToBoolean(String);
-Integer Lookup StringToInteger(String);
-Integer Lookup StringToReal(String);
-String Lookup substr(...);
-Integer Lookup find_first_of(...);
-Integer Lookup find_last_of(...);
+// PLEXIL string operations: see
+// $PLEXIL_HOME/examples/checkpoint/StringAdapter.cc for details, and
+// many more lookups that could be added here if needed.
 
-// Checkpointing interface
-Command set_checkpoint(String,Boolean,String);
-Command flush_checkpoints;
-Command set_boot_ok();
-Integer Lookup CheckpointWhen(String);
-Integer Lookup NumberOfUnhandledBoots;
-Boolean Lookup IsBootOK(Integer);
-Boolean Lookup DidCrash;
+String  Lookup ToString (...);
+Boolean Lookup StringToBoolean (String);
+Integer Lookup StringToInteger (String);
+Integer Lookup StringToReal (String);
+String  Lookup substr (...);
+Integer Lookup find_first_of (...);
+Integer Lookup find_last_of (...);
+
+// PLEXIL checkpointing interface.
+// For details see:
+// https://plexil-group.github.io/plexil_docs/PLEXILExecution/PlanPersistenceandCheckpoints.html
+
+// Overrides any existing checkpoint, returns the previous state of
+// the checkpoint (Unknown if checkpoint did not exist)
+Boolean set_checkpoint (String name, Boolean value=true, String info="");
+
+// Flushes all changes (including set_boot_ok) to disk.
+Boolean flush_checkpoints ();
+
+// Sets IsBootOK, returns previous value
+Integer set_boot_ok (Boolean state=True, Integer boot=0);
+
+// Returns the total number of boots ever logged.
+Integer NumberOfTotalBoots ();
+
+// Returns number of boot entries available to the plan.
+Integer NumberOfAccessibleBoots ();
+
+// Returns number of boot entries which are not marked as is_ok.
+Integer NumberOfUnhandledBoots ();
+
+// Returns true if IsOK(1)==false, false otherwise. A true value
+// indicates that the executive shut down without setting is_ok.
+Boolean DidCrash ();
+
+// If a lookup attempts to query a boot that doesn't exist, Unknown is
+// returned.
+
+// Returns the is_ok flag in the selected boot, which starts off as
+// false when the boot begins and may be affected by the OKOnExit
+// configuration.
+Boolean IsBootOK (Integer boot=0);
+
+// Returns the time that CheckpointAdapter.start() is called by the
+// interface adapter, Unknown if time not available.
+Integer TimeOfBoot (Integer boot=0);
+
+// Returns the time of the last save to disk.
+Integer TimeOfLastSave (Integer boot=0);
+
+// If a lookup attempts to query a checkpoint that doesn't exist,
+// Unknown is returned.
+
+// If checkpoint not set, Unknown.
+Boolean CheckpointState (String name, Integer boot=0);
+
+// Returns last modified time.
+Real CheckpointTime (String name, Integer boot=0);
+
+// User defined string.
+String CheckpointInfo (String name, Integer boot=0);
+
+// Return the most recent boot number where the checkpoint has been
+// set, Unknown if none.
+Integer CheckpointWhen (String name);
 
 #endif
