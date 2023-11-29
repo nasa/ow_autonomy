b1950dc92e5023e8f3513db7e5e0bd80d7faaeb3
Thomas Stucky <thomas.stucky@nasa.gov>
2023-11-28 17:09:41 -0700

    improved code alignment and comments

--- ow_plexil/scripts/identify_sample_location.py
+++ ow_plexil/scripts/identify_sample_location.py
@@ -14,37 +14,39 @@ from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image, PointCloud2
 from geometry_msgs.msg import PointStamped, Point
 import sensor_msgs.point_cloud2
-from ow_plexil.msg import IdentifyLocationAction, IdentifyLocationGoal, IdentifyLocationFeedback, IdentifyLocationResult
+from ow_plexil.msg import IdentifyLocationAction, IdentifyLocationResult
 from visualization_msgs.msg import Marker 
 import message_filters
-from collections import deque
 
 class IdentifySampleLocation:
-  # Constant defining the maximum length of our image history deque.
+
+  # Constant defining the maximum length of our image history list.
   HISTORY_MAX_LENGTH = 50
 
   def __init__(self):
 
     # Set up synchronized subscribers for image_rect and point cloud.
-    rectified_image_subscriber = message_filters.Subscriber("/StereoCamera/left/image_rect_color",
-                                                            Image)
-    point_cloud_subscriber = message_filters.Subscriber("/StereoCamera/points2", PointCloud2)
-    time_synchronizer = message_filters.TimeSynchronizer([rectified_image_subscriber,
-                                                          point_cloud_subscriber], 10)
+    rectified_image_subscriber = message_filters.Subscriber(
+      "/StereoCamera/left/image_rect_color", Image)
+    point_cloud_subscriber = message_filters.Subscriber(
+      "/StereoCamera/points2", PointCloud2)
+    time_synchronizer = message_filters.TimeSynchronizer(
+      [rectified_image_subscriber, point_cloud_subscriber], 10)
     time_synchronizer.registerCallback(self.site_imaging_callback)
 
-    # Publish modified opencv image with the contours and sample location outlined.
-    self.publish_photo = rospy.Publisher('sample_location', Image, queue_size=10)
+    # Publish modified image with the contours and sample location outlined.
+    self.publish_photo = rospy.Publisher(
+      'sample_location', Image, queue_size=10)
 
     # Point visualization
-    self.point_visualization_publisher = rospy.Publisher('sample_point_visualization',
-                                                         Marker, queue_size=10)
+    self.point_visualization_publisher = rospy.Publisher(
+      'sample_point_visualization', Marker, queue_size=10)
 
     # Action server setup
-    self.sample_location_action_server = actionlib.SimpleActionServer("IdentifySampleLocation",
-                                                                      IdentifyLocationAction,
-                                                                      self.sample_location_callback,
-                                                                      False)
+    self.sample_location_action_server = actionlib.SimpleActionServer(
+      "IdentifySampleLocation", IdentifyLocationAction,
+      self.sample_location_callback, False
+    )
     self.sample_location_action_server.start()
 
     # Listener and CvBridge
@@ -60,8 +62,7 @@ class IdentifySampleLocation:
 
 
   def sample_location_callback(self, goal):
-    '''
-    Action Server, goal contains the number of images to be processed
+    '''Action Server, goal contains the number of images to be processed
     as well as the filter type to be used.  Each image gets processed
     and extracts a list of possible 2d sample coordinates and the
     corresponding countours.  The point with the largest contour is
@@ -76,11 +77,11 @@ class IdentifySampleLocation:
     self.sample_location_image = None
     self.uv = None
 
-    # Check if we have enough images to process and if our goal is greater than 0.
+    # Check if we have enough images to process and if goal is positive.
     if len(self.image_history) > 0 and goal.num_images > 0:
       if goal.num_images > len(self.image_history):
-        rospy.logwarn("Goal is larger than total images recorded, only %s images will be processed.",
-                      str(len(self.image_history)))
+        rospy.logwarn("Goal is larger than total images recorded, only "
+                      f"{len(self.image_history)} images will be processed.")
 
       # Take a subset of length goal.num_images of the images in image
       # history for processing.
@@ -88,13 +89,13 @@ class IdentifySampleLocation:
       for i in subset:
         # Get list of pixel coordinates then get the corresponding 3d
         # point with the largest contour.
-        sample_points_2d, contour_areas, cv_image = self.identify_sample_spots(i[0],
-                                                                               goal.filter_type)
-        sample_point_3d, sample_point_area, uv = self.get_3d_sample_point(i[1],
-                                                                          sample_points_2d,
-                                                                          contour_areas)
+        sample_points_2d, contour_areas, cv_image = self.identify_sample_spots(
+          i[0], goal.filter_type)
+        sample_point_3d, sample_point_area, uv = self.get_3d_sample_point(
+          i[1], sample_points_2d, contour_areas)
         # Save the 3d point info with the largest contour area
-        if sample_point_3d is not None and sample_point_area > self.largest_area:
+        if (sample_point_3d is not None
+            and sample_point_area > self.largest_area):
           self.largest_area = sample_point_area
           self.best_sample_location = sample_point_3d
           self.sample_location_image = cv_image
@@ -103,23 +104,24 @@ class IdentifySampleLocation:
       # If succesful we return our point, otherwise we return an empty list.
       if self.best_sample_location == None:
         rospy.loginfo("identify_sample_location.py: location rejected.")
-        self.sample_location_action_server.set_succeeded(IdentifyLocationResult(success=False,
-                                                                                sample_location=None))
+        self.sample_location_action_server.set_succeeded(
+          IdentifyLocationResult(success=False, sample_location=None))
       else:
         # Publish the modified image showing our sample location choice.
         self.publish_chosen_location_image()
         self.visualize_sample_point(self.best_sample_location)
-        self.sample_location_action_server.set_succeeded(IdentifyLocationResult(success=True,
-                                                                                sample_location=self.best_sample_location))
+        self.sample_location_action_server.set_succeeded(
+          IdentifyLocationResult(success=True,
+                                 sample_location=self.best_sample_location)
+        )
 
     else:
-      rospy.loginfo("identify_sample_location.py: No images have been recorded.")
-      self.sample_location_action_server.set_succeeded(IdentifyLocationResult(success=False,
-                                                                              sample_location=None))
+      rospy.loginfo("identify_sample_location.py: No images recorded.")
+      self.sample_location_action_server.set_succeeded(
+        IdentifyLocationResult(success=False, sample_location=None))
 
   def site_imaging_callback(self, rect_img_msg, points2_msg):
-    '''
-    Synchronizes the pointcloud and the rectified image topic and saves
+    '''Synchronizes the pointcloud and the rectified image topic and saves
     them as a tuple to the image_history list.  Starts to delete old
     messages at a length of 50.
     '''
@@ -132,10 +134,8 @@ class IdentifySampleLocation:
       self.image_history = self.image_history[-self.HISTORY_MAX_LENGTH:]
 
   def publish_chosen_location_image(self):
-    '''
-    Publishes the original image with our drawn on contours and chosen
+    '''Publishes the original image with our drawn on contours and chosen
     sample location.  Used to visualize the chosen sample point.
-
     '''
     # Draw a circle where our chosen sample location is located.
     cv.circle(self.sample_location_image, self.uv, 7, (0,255,0),-1)
@@ -148,12 +148,11 @@ class IdentifySampleLocation:
     self.publish_photo.publish(image)
 
   def identify_sample_spots(self, location_image, filter_type):
-    '''
-    Uses OpenCv to process the given image. For filter type Brown we
+    '''Uses OpenCv to process the given image. For filter type Brown we
     isolate a range of brown colors on the image and draw the
     contours. For Dark filter (the default filter) we isolate the
     darkest spots in the image (usually shadows) and find the
-    countours. The 2d center location is taken for these contours and
+    contours. The 2D center location is taken for these contours and
     are sorted in descending order, we then draw/store the largest 5
     contours/center points to return.
     '''
@@ -201,19 +200,19 @@ class IdentifySampleLocation:
     return sample_points_2d, contour_areas, image
 
   def get_3d_sample_point(self, point_cloud, sample_points_2d, contour_areas):
-    '''
-    Given a list of 2d points we find the corresponding 3d point within
-    the pointcloud.  Takes the first valid point (largest contour with
+    '''Given a list of 2D points we find the corresponding 3D point within
+    the point cloud. Takes the first valid point (largest contour with
     valid point) and transforms it into the base_link frame. Returns
-    the new 3d sample point, its corresponding contour and the
-    original 2d location on the image.
+    the new 3D sample point, its corresponding contour and the
+    original 2D location on the image.
     '''
     if sample_points_2d:
       site_coordinate = None
       index = 0
       # Find corresponding 3d point in point cloud from 2d pixel point
       # takes the largest (first valid) point.
-      for i in sensor_msgs.point_cloud2.read_points(point_cloud, uvs=sample_points_2d):
+      for i in sensor_msgs.point_cloud2.read_points(point_cloud,
+                                                    uvs=sample_points_2d):
         if np.isnan(i[0]) or np.isnan(i[1]) or np.isnan(i[2]):
           #recording the index so that we can also get the corresponding contour
           index+=1
